c12729d3dee81f5bc776141d196f59c59a6e4e0d
MegamackDaCodemaster <ac75115@skola.goteborg.s>
2025-11-27 12:34:18 +0100

    Created methods 'setUp' and added content to method 'test2EqualBooks' in class 'BookTest'!

--- src/test/java/se/yrgo/libary/models/BookTest.java
+++ src/test/java/se/yrgo/libary/models/BookTest.java
@@ -2,20 +2,35 @@ package se.yrgo.libary.models;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class BookTest {
+    BookCatalog bc;
+    Book[] bookArray;
+    Book book1;
+    Book book2;
 
+    @BeforeEach
+    void setUp() {
+        bookArray = new Book[10];
+        bc = new BookCatalog();
+        book1 = new Book(1,"Learning Java","","","",0);
+        book2 = new Book(1,"Learning Java","","","",0);
+        bc.addBook(book1);
+        bc.addBook(book2);
+    }
 	//G
 	@Test
 	public void test2EqualBooks() {
-
+        boolean sameBook = book1.equals(book2);
+        assertTrue(sameBook);
 	}
 
 	//G
-	@Test
-	public void test2NonEqualBooks() {
+	//@Test
+	//public void test2NonEqualBooks() {
 
-	}
+	//}
 
 }
